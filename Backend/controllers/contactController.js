bd3fb4a06c72427c30086149bf1bda657db3225d
kedir1996 <kedirahmed19963384@gmail.com>
2026-05-05 13:13:02 +0300

    3333

--- Backend/controllers/contactController.js
+++ Backend/controllers/contactController.js
@@ -12,15 +12,13 @@ export const sendContactEmail = async (req, res) => {
     }
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false, // ለ 587 false መሆን አለበት
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
-      // ይህች ክፍል መታከል አለባት
       family: 4,
     });
 
